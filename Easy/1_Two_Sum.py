c8299758dd9bdc2e6ef2c82de2449fdd61a72374
Julius <juliusfan98@gmail.com>
2019-06-16 15:29:28 -0400

    added folders

---
+++ Easy/1_Two_Sum.py
@@ -0,0 +1,15 @@
+"""
+STATEMENT
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+CLARIFICATIONS
+ - Each input has exactly 1 solution
+ - The same element may not be used twice
+ - The list is not empty
+ - The list is not necessarily sorted
+
+EXAMPLE: 
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1]
+"""
