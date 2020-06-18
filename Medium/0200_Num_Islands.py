10baacc9b67e076e4c86e234902939db61448da0
Julius Fan <juliusfan98@gmail.com>
2020-06-17 22:31:26 -0700

    added description

--- Medium/0200_Num_Islands.py
+++ Medium/0200_Num_Islands.py
@@ -4,20 +4,17 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        def recursiveLandConversion(grid, i, j):
-            # If invalid, return
-            if(i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1'):
+        def recur(grid, i, j):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
                 return
             grid[i][j] = '0'
-            directions = [(0,1), (0,-1), (-1,0), (1,0)]
+            directions = [(0,1), (1,0), (0,-1), (-1,0)]
             for x,y in directions:
-                recursiveLandConversion(grid, i+x, j+y)
-                
-        if len(grid) == 0: return 0
+                recur(grid, i+x, j+y)
         islandCount = 0
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if grid[row][col] == '1':
-                    recursiveLandConversion(grid, row, col)
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == '1':
+                    recur(grid, i, j)
                     islandCount += 1
         return islandCount
